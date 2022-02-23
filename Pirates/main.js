9f863eb9c8d0350bfba9741bc80c0b0eb94e7a4a
Vidit Jain <viditjain@live.com>
2022-02-24 02:09:37 +0530

    Added music code

--- Pirates/main.js
+++ Pirates/main.js
@@ -52,6 +52,20 @@ function init() {
 
 	ship = new Ship(scene);
 	
+	const listener = new THREE.AudioListener();
+	camera.add( listener );
+
+	// create a global audio source
+	const sound = new THREE.Audio( listener );
+
+	// load a sound and set it as the Audio object's buffer
+	const audioLoader = new THREE.AudioLoader();
+	audioLoader.load( 'sounds/pirates.mp3', function( buffer ) {
+		sound.setBuffer( buffer );
+		sound.setLoop( true );
+		sound.setVolume( 0.5 );
+		sound.play();
+	});
 }
 function updateCamera() {
 	if (!ship.obj) return;
